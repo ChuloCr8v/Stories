94f0ad9feace722e89a482638550f7c41fabb76f
chulocr8v <chulocr8v@gmail.com>
2022-08-11 07:44:07 +0100

    update

--- components/UserPost.tsx
+++ components/UserPost.tsx
@@ -1,24 +1,26 @@
-import {FC, useState, useEffect} from 'react'
-import styles from '../styles/UserPost.module.scss'
+import { FC, useState, useEffect } from "react";
+import styles from "../styles/UserPost.module.scss";
 
 interface Props {
-    title: string
-    body: string 
-   timestamp: number 
-   likes: []
-  }
-  
-const UserPost : FC = (post) => {
-  
-  console.log(post.post.title)
-  return (
-      <section className={styles.user_post}>
-        <p className={styles.post_title}> {post.post.title} </p>
-        <p className={styles.post_title}> {post.post.post} </p>
-        <p className={styles.post_title}> {post.post.timestamp} </p>
-        <p className={styles.post_title}> {post.post.likes} </p>
-      </section>
-    )
+  title: string;
+  body: string;
+  timestamp: number;
+  post: string;
+  username: string;
+  posterName: string;
+  posterEmail: string;
+  likes: [];
 }
 
-export default UserPost
+const UserPost: FC = (post: any) => {
+  return (
+    <section className={styles.user_post}>
+      <p className={styles.post_title}> {post.post.title} </p>
+      <p className={styles.post_title}> {post.post.post} </p>
+      <p className={styles.post_title}> {post.post.timestamp} </p>
+      <p className={styles.post_title}> {post.post.likes} </p>
+    </section>
+  );
+};
+
+export default UserPost;
--- components/UserPosts.tsx
+++ components/UserPosts.tsx
@@ -1,52 +1,57 @@
-import {FC, useState, useEffect} from 'react'
-import styles from '../styles/UserPosts.module.scss'
-import {fetchUser} from '../constants/methods'
-import firebase from "firebase/compat/app";
-import { doc, getDoc, getDocs, updateDoc, query, collection, where} from "firebase/firestore";
+import { FC, useState, useEffect } from "react";
+import styles from "../styles/UserPosts.module.scss";
+import { fetchUser } from "../constants/methods";
+import { getDocs, query, collection, where } from "firebase/firestore";
 import { db } from "../constants/firebase";
-import UserPost from './UserPost'
-import Spinner from './Spinner'
+import UserPost from "./UserPost";
+import Spinner from "./Spinner";
 
-const UserPosts : FC = () => {
-  const [loading, setLoading] = useState<any>(true)
-  const [user, setUser] = useState<any>([])
-  const [approvedUserPosts, setApprovedUserPosts] = useState<any>('');
-    
-  
+const UserPosts: FC = () => {
+  const [loading, setLoading] = useState<any>(true);
+  const [user, setUser] = useState<any>([]);
+  const [approvedUserPosts, setApprovedUserPosts] = useState<any>([]);
+
   useEffect(() => {
-    fetchUser(setUser)
-    fetchPosts()
-  }, [])
-  
+    fetchUser(setUser);
+    fetchPosts();
+  }, []);
+
+  setTimeout(() => {
+    fetchPosts();
+  }, 1000);
+
   //Fetch posts
   const fetchPosts = async () => {
+    setLoading(true);
     try {
-      const q = query(collection(db, "posts"), where("username", "==", `${user.username}`)) 
+      const q = query(
+        collection(db, "posts"),
+        where("username", "==", `${user.username}`)
+      );
       const querySnapshot = await getDocs(q);
-      const arr = []
-      console.log(arr)
-      await querySnapshot.forEach((doc) => {
-      console.log('running')
-         // doc.data() is never undefined for query doc snapshots
-         arr.push(doc.data());
-         setApprovedUserPosts(arr)
-         setLoading(false)
-    });
+      const arr = [];
+      querySnapshot.forEach((doc) => {
+        arr.push(doc);
+        setApprovedUserPosts(arr);
+        setLoading(false);
+      });
     } catch (e) {
       console.log(e);
-      alert(e)
+      setLoading(false);
     }
   };
 
- 
   return (
-      <section className={styles.user_posts}>
-        {approvedUserPosts?.length >= 1 ? approvedUserPosts?.map((post) => (
-            <UserPost post={post} />
-        )
-             ) : 'Nothing to show'}   
-      </section>
-    )
-}
- 
-export default UserPosts 
+    <section className={styles.user_posts}>
+      {approvedUserPosts?.length >= 1 ? (
+        approvedUserPosts?.map((post) => (
+          <UserPost post={post.data()} key={post.postId} />
+        ))
+      ) : (
+        <Spinner />
+      )}
+    </section>
+  );
+};
+
+export default UserPosts;
