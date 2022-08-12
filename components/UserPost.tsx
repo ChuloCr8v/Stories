e3df7a402f7d0d5d4c0d6bee326f40cb26a2b4a5
chulocr8v <nkematu5@gmail.com>
2022-08-12 15:47:26 +0100

    updatez

--- components/UserPost.tsx
+++ components/UserPost.tsx
@@ -1,26 +1,38 @@
-import { FC, useState, useEffect } from "react";
-import styles from "../styles/UserPost.module.scss";
+import {FC, useState, useEffect} from 'react'
+import styles from '../styles/UserPost.module.scss'
+import {FaHeart, FaCommentAlt} from 'react-icons/fa'
 
 interface Props {
-  title: string;
-  body: string;
-  timestamp: number;
-  post: string;
-  username: string;
-  posterName: string;
-  posterEmail: string;
-  likes: [];
+    title: string
+    body: string 
+    timestamp: number 
+    likes: []
+  }
+  
+const UserPost : FC = (post) => {
+  
+  console.log(post.post.title)
+  return (
+      <section className={styles.user_post}>
+      <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <p className={styles.post_title}> {post.post.title} </p>
+        <p className={styles.post_body}> {post.post.post} </p>
+        <p className={styles.post_time}> {post.post.timestamp} </p>
+        <div className={styles.reactions_wrapper}>
+        <div className={styles.reaction_wrapper}>
+          <FaHeart className={styles.icon} />
+          <p className={styles.post_likes}> {post.post.likes.length} </p>
+        </div>
+        <div className={styles.reaction_wrapper}>
+          <FaCommentAlt className={styles.icon} />
+          <p className={styles.post_likes}> {post.post.comments.length} </p>
+        </div>
+      </div>
+      </div>
+      </div>
+      </section>
+    )
 }
 
-const UserPost: FC = (post: any) => {
-  return (
-    <section className={styles.user_post}>
-      <p className={styles.post_title}> {post.post.title} </p>
-      <p className={styles.post_title}> {post.post.post} </p>
-      <p className={styles.post_title}> {post.post.timestamp} </p>
-      <p className={styles.post_title}> {post.post.likes} </p>
-    </section>
-  );
-};
-
-export default UserPost;
+export default UserPost
